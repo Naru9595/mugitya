87cdfa4e404da776316fd74f88b7455e79dfc163
noah3139 <noah.621704@gmail.com>
2025-06-16 16:02:00 +0900

    tailwindの設定

--- cafe-order-manage/src/App.jsx
+++ cafe-order-manage/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button class="text-red-500" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
