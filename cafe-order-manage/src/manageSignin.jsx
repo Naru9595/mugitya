a63f8c3402f0c3ac8cc4e1f6dd416541eb6278de
noah3139 <noah.621704@gmail.com>
2025-06-17 11:36:20 +0900

    ログインページの仮完成

--- cafe-order-manage/src/manageSignin.jsx
+++ cafe-order-manage/src/manageSignin.jsx
@@ -3,12 +3,32 @@ function ManageSignin(){
         <>
             <div className="h-screen bg-gray-200">
                 <div className="flex h-screen items-center justify-center">
-                    <div  className="bg-white">
-                        <div className="text-xl text-blue-700">管理者としてログイン</div>
-                        <div className="text-xs">メールアドレス</div>
-                        <input type="email" placeholder="kosentarou@gmail.com"/>
-                        <div className="text-xs">パスワード</div>
-                        <input type="password" placeholder="KosenTarou-01"/>
+                    <div  className="bg-white rounded">
+                        <div className="text-xl text-blue-700 p-2">管理者としてログイン</div>
+                        <div className="px-2 pb-2">
+                            <div className="text-xs">メールアドレス</div>
+                            <div>
+                                <input type="email" placeholder="kosentarou@gmail.com" className="w-full rounded border border-gray-500"/>
+                            </div>
+                        </div>
+                        <div className="px-2 pb-2">
+                            <div className="text-xs">パスワード</div>
+                            <div>
+                                <input type="password" placeholder="KosenTarou-01" className="w-full rounded border border-gray-500"/>
+                            </div>
+                        </div>
+                        <div className="flex justify-center text-sm rounded mx-2 mt-2 mb-3 py-1 bg-blue-500 hover:bg-blue-600">
+                            <button>
+                                ログイン
+                            </button>
+                        </div>
+                        <div className="border-t border-dashed border-gray-500">
+                            <div className="flex justify-center text-sm rounded mx-2 my-3 py-1 bg-blue-500 hover:bg-blue-600">
+                                <button>
+                                    新規登録
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
