5f2f20ab14d33b836c1b1b0f5ff2f4f03639d67e
noah3139 <noah.621704@gmail.com>
2025-06-18 14:05:55 +0900

    サイドバーのボタンの修正

--- cafe-order-manage/src/manageSidebar.jsx
+++ cafe-order-manage/src/manageSidebar.jsx
@@ -2,24 +2,24 @@ function ManageSidebar(){
     return(
         <>
             <div className="w-40 text-white bg-slate-800 h-screen">
-                <div className="p-2 text-base">
-                    <button>管理メニュー</button>
+                <div className="p-4 text-base">
+                    管理メニュー
                 </div>
-                <div className="p-4 text-xs">
+                <div className="p-4 text-xs hover:bg-slate-700">
                     <button>注文管理</button>
                 </div>
-                <div className="p-4 text-xs">
+                <div className="p-4 text-xs hover:bg-slate-700">
                     <button>メニュー・在庫管理</button>
                 </div>
-                <div className="p-4 text-xs">
+                <div className="p-4 text-xs hover:bg-slate-700">
                     <button>売り上げ分析</button>
                 </div>
-                <div className="p-4 text-xs">
+                <div className="p-4 text-xs hover:bg-slate-700">
                     <button>設定</button>
                 </div>
                 <div className="p-4 absolute bottom-4 text-xs">
                     <div>kosentarou@gmail.com</div>
-                    <div className="text-red-300">
+                    <div className="text-red-500 hover:text-red-400">
                         <button>ログアウト</button>
                     </div>
                 </div>
