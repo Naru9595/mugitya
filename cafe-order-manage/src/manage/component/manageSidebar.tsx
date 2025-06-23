6b328771effad76fae58ba9db4206beb6099d9b5
noah3139 <noah.621704@gmail.com>
2025-06-23 15:34:22 +0900

    管理者側のログアウトボタンの実装

--- cafe-order-manage/src/manage/component/manageSidebar.tsx
+++ cafe-order-manage/src/manage/component/manageSidebar.tsx
@@ -27,9 +27,9 @@ function ManageSidebar(){
                 </div>
                 <div className="p-4 absolute bottom-4 text-xxs">
                     <div>k00000xx@xxxx.xxx.ac.jp</div>
-                    <div className="w-20 text-red-500 hover:text-red-400">
-                        <button>ログアウト</button>
-                    </div>
+                    <a href="./manageSignin" className="w-20 text-red-500 hover:text-red-400">
+                        ログアウト
+                    </a>
                 </div>
             </div>
         </>
