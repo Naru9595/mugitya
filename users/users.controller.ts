dbf3c34b5496921ce8837657922ee8dba780a0d8
NARUPC\knaru <narunaru9595@gmail.com>
2025-07-14 16:03:59 +0900

    ろぐいんせいこうです

--- users/users.controller.ts
+++ users/users.controller.ts
@@ -1,20 +1,35 @@
-
-import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
-import { User } from './user';
+import { Body, Controller, Get, Post, Request, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
+
   @Post()
   create(@Body(ValidationPipe) user: User) {
     return this.usersService.create(user);
   }
 
-}
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+}
