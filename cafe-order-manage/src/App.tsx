f6bd6a082e0563f7921dddbd3eee1a55c279c6d0
NARUPC\knaru <narunaru9595@gmail.com>
2025-07-15 15:36:49 +0900

    ろぐいんごのせんいかんりょう

--- cafe-order-manage/src/App.tsx
+++ cafe-order-manage/src/App.tsx
@@ -1,7 +1,7 @@
-// import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useAuth } from './AuthContext';
 
-// import './App.css'
 import './index.css'
 import ManageSignin from "./manage/manageSignin/page"
 import ManageSignup from "./manage/manageSignup/page"
@@ -13,24 +13,42 @@ import UserSignin from './user/userSignin/page'
 import UserSignup from './user/userSignup/page'
 import UserMenu from './user/userMenu/page'
 
-function App() {
+function AppContent() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      if (user.role === 'admin') {
+        navigate('/orderManage');
+      } else {
+        navigate('/userMenu');
+      }
+    }
+  }, [user, navigate]);
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/manageSignin" element={<ManageSignin/>}/>
-          <Route path="/manageSignup" element={<ManageSignup/>}/>
-          <Route path="/menuManage" element={<MenuManage/>}/>
-          <Route path="/orderManage" element={<OrderManage/>}/>
-          <Route path="/salesAnalyze" element={<SalesAnalyze/>}/>
-          <Route path="/manageSetting" element={<ManageSetting/>}/>
-          <Route path="/Signin" element={<UserSignin/>}/>
-          <Route path="/userSignup" element={<UserSignup/>}/>
-          <Route path="/userMenu" element={<UserMenu/>}/>
-        </Routes>
-      </Router>
-    </>
-  )
+    <Routes>
+      <Route path="/manageSignin" element={<ManageSignin/>}/>
+      <Route path="/manageSignup" element={<ManageSignup/>}/>
+      <Route path="/menuManage" element={<MenuManage/>}/>
+      <Route path="/orderManage" element={<OrderManage/>}/>
+      <Route path="/salesAnalyze" element={<SalesAnalyze/>}/>
+      <Route path="/manageSetting" element={<ManageSetting/>}/>
+      <Route path="/Signin" element={<UserSignin/>}/>
+      <Route path="/userSignup" element={<UserSignup/>}/>
+      <Route path="/userMenu" element={<UserMenu/>}/>
+      <Route path="/" element={<UserSignin />} />
+    </Routes>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
