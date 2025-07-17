6395b6d0b8ad1f63242f716d28ef595d530b23f2
noah3139 <noah.621704@gmail.com>
2025-07-17 09:44:18 +0900

    トップ画面を作成しました

--- cafe-order-manage/src/App.tsx
+++ cafe-order-manage/src/App.tsx
@@ -13,11 +13,36 @@ import UserSignin from './user/userSignin/page'
 import UserSignup from './user/userSignup/page'
 import UserMenu from './user/userMenu/page'
 
+import { Link } from 'react-router-dom'
+
+function TopPage() {
+  return (
+    <div className="relative w-screen h-screen bg-white">
+      {/* 表示用の背景やテキスト */}
+      <div className="absolute inset-0 flex items-center justify-center z-0">
+        <div>
+          <div className="text-2xl text-blue-700 font-bold">学食スマートオーダー</div>
+          <div className="flex items-center justify-center mt-2">タップしてログイン</div>
+        </div>
+      </div>
+
+      {/* 全画面リンク */}
+      <Link
+        to="/Signin"
+        className="absolute inset-0 z-10"
+      >
+        {/* 中身は空でOK、全画面クリック用 */}
+      </Link>
+    </div>
+  )
+}
+
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<TopPage/>}/>
           <Route path="/manageSignin" element={<ManageSignin/>}/>
           <Route path="/manageSignup" element={<ManageSignup/>}/>
           <Route path="/menuManage" element={<MenuManage/>}/>
